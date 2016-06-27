28552c6d68d34573fc734b75a23c572680792925
Richard Szalay <richard@richardszalay.com>
2016-06-28 08:06:04 +1000

    Upgraded netcore support to GA / RTM

--- DownloadNetCoreSdk.ps1
+++ DownloadNetCoreSdk.ps1
@@ -3,6 +3,6 @@ param([String]$ToolsPath)
 if (Test-Path "$ToolsPath") { remove-item -Recurse -Force "$ToolsPath" }
 mkdir .dotnetcli | Out-Null
 
-wget https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.ps1 -OutFile "$ToolsPath\install.ps1"
+wget https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.ps1 -OutFile "$ToolsPath\install.ps1"
 
 .\.dotnetcli\install.ps1 -Channel preview -version Latest -InstallDir "$ToolsPath" -NoPath
