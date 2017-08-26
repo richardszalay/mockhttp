822c00d76958f62a20c56c7d89a65dfdf80b1e8b
Richard Szalay <richard@richardszalay.com>
2017-08-26 21:47:42 +1000

    Added BackendDefinitionBehavior to allow matching Backend Definitions when Request Expecations exist, but don't match

---
+++ RichardSzalay.MockHttp.Shared/BackendDefinitionBehavior.cs
@@ -0,0 +1,17 @@
+﻿namespace RichardSzalay.MockHttp
+{
+    /// <summary>
+    /// Defines the behavior for processing BackendDefinitions when Request Expectations exist
+    /// </summary>
+    public enum BackendDefinitionBehavior
+    {
+        /// <summary>
+        /// Will not match a Backend Definition if Request Expectations exist
+        /// </summary>
+        NoExpectations = 0,
+        /// <summary>
+        /// Will match Backend Definition if the next Request Expectation did not match
+        /// </summary>
+        Always = 1
+    }
+}
