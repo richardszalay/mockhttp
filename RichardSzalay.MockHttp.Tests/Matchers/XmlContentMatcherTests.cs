1c9e3bf7dfd04b4a2082d90f1fb3d07c8309d09c
Richard Szalay <richard@richardszalay.com>
2023-08-12 20:43:02 +1000

    Add WithXmlContent matchers
      * `WithXmlContent<T>(T value, XmlSerializer? options = null,
    XmlWriterSettings? settings = null)` - serialize the input and then
    match on that exact string content in the request
      * `WithXmlContent<T>(Func<T, bool> value, XmlSerializer? options =
        null)` - deserialize the request content and then match the result
        against the provided predicate.

---
+++ RichardSzalay.MockHttp.Tests/Matchers/XmlContentMatcherTests.cs
@@ -0,0 +1,57 @@
+﻿using RichardSzalay.MockHttp.Matchers;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace RichardSzalay.MockHttp.Tests.Matchers
+{
+    public class XmlContentMatcherTests
+    {
+        private static XmlSerializer xmlSerializer = new XmlSerializer(typeof(XmlContent));
+
+        [Fact]
+        public void Should_succeed_when_predicate_returns_true()
+        {
+            var result = Test(
+                expected: c => c.Value == true,
+                actual: new XmlContent { Value = true }
+                );
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_fail_when_predicate_returns_false()
+        {
+            var result = Test(
+                expected: c => c.Value == false,
+                actual: new XmlContent { Value = true }
+                );
+
+            Assert.False(result);
+        }
+
+        private bool Test(Func<XmlContent, bool> expected, XmlContent actual)
+        {
+            var sut = new XmlContentMatcher<XmlContent>(expected);
+
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms);
+            xmlSerializer.Serialize(sw, actual);
+
+            StringContent content = new StringContent(Encoding.UTF8.GetString(ms.ToArray()));
+
+            return sut.Matches(new HttpRequestMessage(HttpMethod.Get,
+                "http://tempuri.org/home")
+            { Content = content });
+        }
+
+        public class XmlContent
+        {
+            public bool Value { get; set; }
+        }
+    }
+}
