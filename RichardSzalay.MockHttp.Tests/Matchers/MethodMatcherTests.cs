47f9970fff593058d644a890da83f59f8177c65e
Lars Krämer <lars.kraemer@live.de>
2023-09-26 18:22:32 +0200

    MHTTP-1: added additional tests

---
+++ RichardSzalay.MockHttp.Tests/Matchers/MethodMatcherTests.cs
@@ -0,0 +1,36 @@
+using FluentAssertions;
+using RichardSzalay.MockHttp.Matchers;
+
+namespace RichardSzalay.MockHttp.Tests.Matchers;
+
+[TestClass]
+public class MethodMatcherTests
+{
+    [TestMethod]
+    public void ShouldSucceedOnMatchedMethod()
+    {
+        bool result = Test(
+            expected: HttpMethod.Get,
+            actual: HttpMethod.Get
+        );
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void ShouldFailOnMismatchedMethod()
+    {
+        bool result = Test(
+            expected: HttpMethod.Get,
+            actual: HttpMethod.Post
+        );
+        
+        result.Should().BeFalse();
+    }
+
+    private bool Test(HttpMethod expected, HttpMethod actual)
+    {
+        return new MethodMatcher(expected)
+            .Matches(new HttpRequestMessage(actual, "http://tempuri.org/home"));
+    }
+}
