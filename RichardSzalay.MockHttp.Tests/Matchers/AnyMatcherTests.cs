87b40effa0d3a9c502f2842920a2768c54db81ae
Lars Krämer <lars.kraemer@live.de>
2023-09-26 19:00:52 +0200

    MHTTP-1: added additional tests

---
+++ RichardSzalay.MockHttp.Tests/Matchers/AnyMatcherTests.cs
@@ -0,0 +1,56 @@
+using FluentAssertions;
+using RichardSzalay.MockHttp.Contracts;
+using RichardSzalay.MockHttp.Matchers;
+
+namespace RichardSzalay.MockHttp.Tests.Matchers;
+
+[TestClass]
+public class AnyMatcherTests
+{
+    [TestMethod]
+    public void SucceedsIfFirstMatcherSucceeds()
+    {
+        var result = Test(true, false);
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void SucceedsIfLastMatcherSucceeds()
+    {
+        var result = Test(false, true);
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void FailsIfAllMatchersFail()
+    {
+        var result = Test(false, false);
+
+        result.Should().BeFalse();
+    }
+
+    public bool Test(params bool[] matcherResults)
+    {
+        var matchers = matcherResults
+            .Select(result => new FakeMatcher(result));
+
+        return new AnyMatcher(matchers)
+            .Matches(new HttpRequestMessage());
+    }
+
+    private class FakeMatcher : IMockedRequestMatcher
+    {
+        private bool _result;
+        public FakeMatcher(bool result)
+        {
+            this._result = result;
+        }
+
+        public bool Matches(System.Net.Http.HttpRequestMessage message)
+        {
+            return _result;
+        }
+    }
+}
