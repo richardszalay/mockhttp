167c603cdca45227f629ba0af47b80a075333101
Lars Krämer <lars.kraemer@live.de>
2023-09-26 18:41:56 +0200

    MHTTP-1: added additional tests

---
+++ RichardSzalay.MockHttp.Tests/Infrastructure/HttpHelpers.cs
@@ -0,0 +1,15 @@
+namespace RichardSzalay.MockHttp.Tests.Infrastructure;
+
+public static class HttpHelpers
+{
+    internal static IEnumerable<KeyValuePair<string, string>> ParseQueryString(string input)
+    {
+        return input.TrimStart('?').Split('&')
+            .Select(pair => pair.Split(new [] { '=' }, 2))
+            .Select(pair => new KeyValuePair<string, string>(
+                Uri.UnescapeDataString(pair[0]),
+                pair.Length == 2 ? Uri.UnescapeDataString(pair[1]) : null
+            ))
+            .ToList();
+    }
+}
---
+++ RichardSzalay.MockHttp.Tests/Matchers/FormDataMatcherTests.cs
@@ -0,0 +1,148 @@
+using System.Net.Http.Headers;
+using FluentAssertions;
+using RichardSzalay.MockHttp.Matchers;
+using RichardSzalay.MockHttp.Tests.Infrastructure;
+
+namespace RichardSzalay.MockHttp.Tests.Matchers;
+
+[TestClass]
+public class FormDataMatcherTests
+{
+    [TestMethod]
+    [DataRow("key1=value1&key2=value2", "key1=value1&key2=value2", false, true)]
+    [DataRow("key2=value2&key1=value1", "key1=value1&key2=value2", false, true)]
+    [DataRow("key1=value1&key1=value2", "key1=value2&key1=value1", false, true)]
+    [DataRow("key2=value2&key1", "key1&key2=value2", false, true)]
+    [DataRow("key1=value1&key2=value3", "key1=value1&key2=value2", false, false)]
+    [DataRow("key2=value2&key1=value1", "key1=value1&key3=value3", false, false)]
+    [DataRow("key1=value1&key2=value2", "key1=value1&key2=value2&key3=value3", false, true)]
+    [DataRow("key1=value1&key2=value2", "key1=value1&key2=value2&key3=value3", true, false)]
+    public void Matches_WithData_Returns(string expected, string actual, bool exact, bool expectedResult)
+    {
+        FormUrlEncodedContent content = new(
+            HttpHelpers.ParseQueryString(actual)
+        );
+
+        var result = new FormDataMatcher(expected, exact)
+            .Matches(new HttpRequestMessage(HttpMethod.Get, "http://tempuri.org/home")
+            {
+                Content = content
+            });
+
+        result.Should().Be(expectedResult);
+    }
+
+    /// <summary>
+    /// FormDataMatcher.Matches() should match dictionary data with URL encoded query string values.
+    /// </summary>
+    [TestMethod]
+    public void ShouldSupportMatchingDictionaryDataWithUrlEncodedValues1()
+    {
+        var data = new Dictionary<string, string>
+        {
+            { "key", "Value with spaces" }
+        };
+
+        var content = new FormUrlEncodedContent(data);
+        var sut = new FormDataMatcher(data);
+
+        var actualMatch = sut.Matches(new HttpRequestMessage(HttpMethod.Get, "http://tempuri.org/home")
+        {
+            Content = content
+        });
+
+        actualMatch.Should().BeTrue();
+    }
+
+    /// <summary>
+    /// FormDataMatcher.Matches() should match dictionary data with URL encoded query string values.
+    /// </summary>
+    [TestMethod]
+    public void ShouldSupportMatchingDictionaryDataWithUrlEncodedValues2()
+    {
+        var content = new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            { "key", "Value with spaces" }
+        });
+        var sut = new FormDataMatcher("key=Value+with%20spaces");
+
+        var actualMatch = sut.Matches(new HttpRequestMessage(HttpMethod.Get, "http://tempuri.org/home")
+        {
+            Content = content
+        });
+
+        actualMatch.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Should_fail_for_non_form_data()
+    {
+        var content = new FormUrlEncodedContent(HttpHelpers.ParseQueryString("key=value"));
+        content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+
+        var result = Test(
+            expected: "key=value",
+            actual: content
+        );
+
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Supports_multipart_formdata_content()
+    {
+        var content = new MultipartFormDataContent
+        {
+            new FormUrlEncodedContent(HttpHelpers.ParseQueryString("key=value"))
+        };
+
+        var result = Test(
+            expected: "key=value",
+            actual: content
+        );
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Matches_form_data_across_multipart_entries()
+    {
+        var content = new MultipartFormDataContent
+        {
+            new FormUrlEncodedContent(HttpHelpers.ParseQueryString("key1=value1")),
+            new FormUrlEncodedContent(HttpHelpers.ParseQueryString("key2=value2"))
+        };
+
+        var result = Test(
+            expected: "key1=value1&key2=value2",
+            actual: content
+        );
+
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Does_not_match_form_data_on_non_form_data_multipart_entries()
+    {
+        var content = new MultipartFormDataContent
+        {
+            new FormUrlEncodedContent(HttpHelpers.ParseQueryString("key1=value1")),
+            new FormUrlEncodedContent(HttpHelpers.ParseQueryString("key2=value2"))
+        };
+
+        content.First().Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+
+        var result = Test(
+            expected: "key1=value1&key2=value2",
+            actual: content
+        );
+
+        result.Should().BeFalse();
+    }
+
+    private bool Test(string expected, HttpContent actual) => new FormDataMatcher(expected)
+        .Matches(new HttpRequestMessage(HttpMethod.Get, "http://tempuri.org/home")
+        {
+            Content = actual
+        });
+}
