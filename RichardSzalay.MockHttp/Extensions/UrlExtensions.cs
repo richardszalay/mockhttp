c8ec52402bbed7c89096804a031f2495a3e31725
Lars Krämer <lars.kraemer@live.de>
2023-09-27 10:36:53 +0200

    MHTTP1: changes from sonar analytics
    nullable

--- RichardSzalay.MockHttp/Extensions/UrlExtensions.cs
+++ RichardSzalay.MockHttp/Extensions/UrlExtensions.cs
@@ -4,18 +4,24 @@ public static class UrlExtensions
 {
     public static bool IsWellFormedUriString(this string url, UriKind kind)
     {
-        Uri output;
+        Uri? output;
         return TryParse(url, kind, out output);
     }
 
     // MonoAndroid parses relative URI's and adds a "file://" protocol, which causes the matcher to fail
-    public static bool TryParse(this string url, UriKind kind, out Uri output)
+    public static bool TryParse(this string url, UriKind kind, out Uri? output)
     {
-        bool systemResult = Uri.TryCreate(url, kind, out output);
+        Uri.TryCreate(url, kind, out output);
 
-        bool isAndroidFalsePositive = systemResult && output.Scheme == "file" &&
-                                      !url.StartsWith("file://", StringComparison.Ordinal);
+        if (output is null)
+        {
+            return false;
+        }
+        
+        bool isAndroidFalsePositive = output.Scheme == "file"
+                                      && !url.StartsWith("file://", StringComparison.Ordinal);
 
-        return systemResult && !isAndroidFalsePositive;
+        return !isAndroidFalsePositive;
+
     }
 }
